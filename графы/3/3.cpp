dad1923944266623739681d3a74436c104d2e5ae
EgorChernishevskiy <100187552+EgorChernishevskiy@users.noreply.github.com>
2022-05-16 20:22:29 +0400

    task 3
    Дан ориентированный граф. Подсчитать полустепень захода каждой вершины.

---
+++ графы/3/3.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+	int N, M; //кол-во вершин и рёбер
+	cout << "Enter N, M: ";
+	cin >> N >> M; //ввод кол-ва рёбер и вершин
+	int x, y; //две вершины 
+	vector<vector<int>> gr;
+	gr.resize(N);
+	for (int i = 0; i < M; i++) {
+		cin >> x >> y;
+		if (x < N && y < N) {
+			gr[y].push_back(x);
+		}
+	}
+	for (int i = 0; i < gr.size(); i++) {
+		sort(gr[i].begin(), gr[i].end());
+		gr[i].erase(unique(gr[i].begin(), gr[i].end()), gr[i].end());
+	}
+
+
+	for (int i = 0; i < gr.size(); i++) {
+		cout << gr[i].size() << ' ';
+	}
+  return 0;
+}
